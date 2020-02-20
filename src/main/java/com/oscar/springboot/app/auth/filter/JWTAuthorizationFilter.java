9fda88fc325ebe76e2bb9bc1563523773ccd6e2e
Oscar Luna <oscarluna.dev93@gmail.com>
2020-02-20 12:34:57 -0600

    filtro de autorizacion

---
+++ src/main/java/com/oscar/springboot/app/auth/filter/JWTAuthorizationFilter.java
@@ -0,0 +1,40 @@
+package com.oscar.springboot.app.auth.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter{
+
+	public JWTAuthorizationFilter(AuthenticationManager authenticationManager) {
+		super(authenticationManager);
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		String header = request.getHeader("Authorization");
+		
+		if (!requiresAuthentication(header)) {
+			chain.doFilter(request, response);
+			return;
+		}
+	}
+	
+	protected boolean requiresAuthentication(String header) {
+		if (header == null || !header.startsWith("Bearer ")) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	
+}
