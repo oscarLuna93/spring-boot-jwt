d6b0f01c9ff4dc5e41632e174e86eea0635d8c66
Oscar Luna <oscarluna.dev93@gmail.com>
2020-02-20 23:50:16 -0600

    se cambia firma de metodos

--- src/main/java/com/oscar/springboot/app/auth/service/JWTService.java
+++ src/main/java/com/oscar/springboot/app/auth/service/JWTService.java
@@ -1,19 +1,18 @@
 package com.oscar.springboot.app.auth.service;
 
+import java.io.IOException;
 import java.util.Collection;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
 import io.jsonwebtoken.Claims;
 
 public interface JWTService {
-	public String create(Authentication auth) throws JsonProcessingException;
+	public String create(Authentication auth) throws IOException;
 	public boolean validate(String token);
 	public Claims getClaims(String token);
 	public String getUsername(String token);
-	public Collection<? extends GrantedAuthority> getRoles(String token);
+	public Collection<? extends GrantedAuthority> getRoles(String token) throws IOException;
 	public String resolve(String token);
 }
