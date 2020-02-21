6f0cc9901471d496fa5a27bd56e14c7ce911e64a
Oscar Luna <oscarluna.dev93@gmail.com>
2020-02-20 22:59:25 -0600

    modificacion metodo create

--- src/main/java/com/oscar/springboot/app/auth/service/JWTService.java
+++ src/main/java/com/oscar/springboot/app/auth/service/JWTService.java
@@ -5,10 +5,12 @@ import java.util.Collection;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import io.jsonwebtoken.Claims;
 
 public interface JWTService {
-	public String create(Authentication auth);
+	public String create(Authentication auth) throws JsonProcessingException;
 	public boolean validate(String token);
 	public Claims getClaims(String token);
 	public String getUsername(String token);
